6fbad81963cde81bfe5405e3ab0984f86b777494
camiloJara0 <camilojara0000@gmail.com>
2025-04-24 18:39:38 -0500

    Proyecto

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,7 +6,7 @@ export const projects: Project[] = [
     title: "Cofilm",
     description: "Plataforma web que se conecta a una API de películas, permitiendo buscar películas y series, ver trailers y explorar detalles de cada título.",
     technologies: ["HTML", "Javascript", "API", "Bootstrap"],
-    imageUrl: "../../dist/assets/cofilm.png",
+    imageUrl: "../../img/cofilm.png",
     githubUrl: "https://github.com/username/ecommerce-platform"
   },
   {
@@ -14,7 +14,7 @@ export const projects: Project[] = [
     title: "Pagina E-commerce",
     description: "Tienda en línea especializada en bicicletas, con frontend y backend integrados para la gestión de productos, visualización de catálogo y compras.",
     technologies: ["React", "Redux", "Firebase", "Tailwind CSS", "Node.js"],
-    imageUrl: "../../dist/assets/bikestore.png",
+    imageUrl: "../../img/bikestore.png",
     githubUrl: "https://github.com/Luiis24/Bike-Store.git"
   },
   {
@@ -22,7 +22,7 @@ export const projects: Project[] = [
     title: "Gestor de mantenimientos",
     description: "Sistema web para estudiantes que permite registrar hojas de vida, reportar mantenimientos a maquinaria industrial y gestionar insumos y herramientas.",
     technologies: ["React", "Javascript", "Node.js", "PostgreSQL", "Tailwind CSS"],
-    imageUrl: "../../dist/assets/SGMI.png",
+    imageUrl: "../../img/SGMI.png",
     githubUrl: "https://github.com/Luiis24/Sistema-Para-La-Gestion-De-Mantenimiento.git"
   }
 ];
